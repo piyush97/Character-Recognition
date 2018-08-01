ed19396b0b82e3d05e48fa36e9f504df6623b131
Piyush Mehta <me@piyushmehta.com>
2018-08-01 14:11:59 +0530

    Main.js
    Signed-off-by: Piyush Mehta <me@piyushmehta.com>

---
+++ main.js
@@ -0,0 +1,30 @@
+let model;
+
+const modelURL = 'http://localhost:5000/model';
+
+const preview = document.getElementById("preview");
+const predictButton = document.getElementById("predict");
+const clearButton = document.getElementById("clear");
+const numberOfFiles = document.getElementById("number-of-files");
+const fileInput = document.getElementById('file');
+
+const predict = async (modelURL) => {
+    if (!model) model = await tf.loadModel(modelURL);
+    const files = fileInput.files;
+
+    [...files].map(async (img) => {
+        const data = new FormData();
+        data.append('file', img);
+
+        const processedImage = await fetch("/api/prepare",
+            {
+                method: 'POST',
+                body: data
+            }).then(response => {
+                return response.json();
+            }).then(result => {
+                return tf.tensor2d(result['image']);
+            });
+
+    })
+};
